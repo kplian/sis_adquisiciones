f45217ba90afb610940469f53d4c4e8f47f57e80
Administrator <Administrator@endetransmision.bo>
2020-10-16 09:17:24 -0400

    #ETR-1393 Cambio de depto a central

--- base/dataupdate000001.sql
+++ base/dataupdate000001.sql
@@ -1,3 +1,13 @@
 /********************************************I-DAUP-AUTOR-SCHEMA-0-31/02/2019********************************************/
 --SHEMA : Esquema (CONTA) contabilidad         AUTHOR:Siglas del autor de los scripts' dataupdate000001.txt
 /********************************************F-DAUP-AUTOR-SCHEMA-0-31/02/2019********************************************/
+/********************************************I-DAUP-EGS-ADQ-0-16/10/2020********************************************/
+--Dato original
+-- update adq.tsolicitud set
+--     id_depto=16
+-- WHERE id_solicitud = 6273;
+--Cambio
+update adq.tsolicitud set
+    id_depto=5
+WHERE id_solicitud = 6273;
+/********************************************F-DAUP-EGS-ADQ-0-16/10/2020********************************************/
